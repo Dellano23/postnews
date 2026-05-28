f745298dfa95fe043e5ff8280cac06f5b0ff6a48
Dellano <124741424+Dellano23@users.noreply.github.com>
2026-05-28 16:35:44 +0000

    Removing  from  for deployment

--- portfolio-next/app/api/v1/migrations/route.js
+++ portfolio-next/app/api/v1/migrations/route.js
@@ -1,10 +1,5 @@
 import { NextResponse } from "next/server";
-import migrationRunner from "node-pg-migrate";
 
 export async function GET(request) {
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-  });
   return NextResponse.json(migrations);
 }
